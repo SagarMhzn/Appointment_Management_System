49f838550875bb3763a7b09aaf095bac74c2d61d
SagarMhzn <maharzansagar4u@gmail.com>
2023-06-01 16:25:25 +0545

    Project commences

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\SuperAdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/checkrole')->middleware('role')->name('checkrole');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// Route::get('/admin/home', [SuperAdminController::class, 'index'])->name('superadmin.home');
+Route::get('/client/home', [ClientController::class, 'index'])->name('client.home');
+Route::get('/doctor/home', [DoctorController::class, 'index'])->name('doctor.home');
+
+
+Route::name('client.')->group(function () {
+    Route::get('/client/home', [SuperAdminController::class,'index'])->name('home');
+    Route::resource('client', ClientController::class);
+});
+
+Route::name('doctor.')->group(function () {
+    Route::get('/doctor/home', [SuperAdminController::class,'index'])->name('home');
+    Route::resource('doctor', DoctorController::class);
+});
+
+Route::name('superadmin.')->group(function () {
+    Route::get('/admin/home', [SuperAdminController::class,'index'])->name('home');
+    Route::resource('superadmin', SuperAdminController::class);
+});
